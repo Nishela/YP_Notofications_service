c956e60c377e6700ce82014d2922618e949168e9
sergey.aleshin <serega_aleshin@mail.ru>
2022-11-23 22:08:44 +0300

    Иницализация БД через alembic. Добавил Makefile, readme

---
+++ Makefile
@@ -0,0 +1,29 @@
+THIS_FILE := $(lastword $(MAKEFILE_LIST))
+.PHONY: help build up start down destroy stop restart logs logs-api ps login-timesc
+
+build:
+	docker-compose build $(c)
+
+up:
+	docker-compose up -d $(c)
+
+down:
+	docker-compose down
+
+run:
+	docker-compose up -d --build
+
+destroy:
+	docker-compose down -v $(c)
+
+stop:
+	docker-compose stop
+
+start:
+	docker-compose start
+
+logs:
+	docker-compose logs --tail=100
+
+init_db:
+	docker exec -it notification-api alembic upgrade head
--- notification_api_service/source/main.py
+++ notification_api_service/source/main.py
@@ -1,3 +1,5 @@
+import logging
+
 import aio_pika
 import uvicorn
 from fastapi import FastAPI
@@ -5,7 +7,6 @@ from fastapi.responses import ORJSONResponse
 
 from api.v1 import notifications
 from core.config import get_settings
-from database.fill_db import init_tables, init_notifications
 from rabbit_producer import rabbit_utils
 from rabbit_producer.producer import RabbitProducer
 
@@ -23,8 +24,6 @@ app = FastAPI(
 async def startup():
     rabbit_utils.mq_connection = await aio_pika.connect_robust(**settings.rabbit_config.dict())
     rabbit_utils.mq_producer = await RabbitProducer().async_configure(settings.rabbit_config.EXCHANGE_POINT_NAME)
-    await init_tables()
-    await init_notifications()
 
 
 @app.on_event('shutdown')
@@ -40,6 +39,6 @@ if __name__ == '__main__':
         host='0.0.0.0',
         reload=True,
         port=8001,
-        # log_config=settings.app.logging,
-        # log_level=logging.DEBUG,
+        log_config=settings.app.logging,
+        log_level=logging.DEBUG,
     )
